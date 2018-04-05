73f6e62e687a1cc7cc2cf1877a9d4a96d4121be0
JUrias117 <33735573+JUrias117@users.noreply.github.com>
2018-04-05 16:48:18 -0700

    Update Basic-info-script.ps1
    Added ipconfig and adds ipv4 address to text file
    Added ping test to google.com and adds results summary to text file

--- Basic-info-script.ps1
+++ Basic-info-script.ps1
@@ -1,3 +1,5 @@
-﻿$name = hostname 
+$name = hostname 
 new-item "$name.txt"
 $name | add-content "$name.txt"
+$(ipconfig | where {$_ -match 'IPv4.+\s(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' } | out-null; $Matches[1])|add-content "$name.txt"
+ping google.com | where {$_ -match 'Minimum'} |add-content "$name.txt"
