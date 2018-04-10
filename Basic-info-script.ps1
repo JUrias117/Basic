95cc443ad10de14ab440802f2215f2f39e1ac30f
JUrias117 <33735573+JUrias117@users.noreply.github.com>
2018-04-10 12:40:03 -0700

    Add files via upload
    Added time, osversion repair volume prompt, defrag prompt,

--- Basic-info-script.ps1
+++ Basic-info-script.ps1
@@ -1,5 +1,22 @@
-$name = hostname 
-new-item "$name.txt"
-$name | add-content "$name.txt"
-$(ipconfig | where {$_ -match 'IPv4.+\s(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' } | out-null; $Matches[1])|add-content "$name.txt"
-ping google.com | where {$_ -match 'Minimum'} |add-content "$name.txt"
+﻿$name = hostname #set variable to machine name
+
+new-item "$name.txt" #create txt file
+$name | add-content "$name.txt" #add name to file
+Get-Date -Format g | Add-Content "$name.txt" #add date/time 
+$(ipconfig | where {$_ -match 'IPv4.+\s(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' } | out-null; $Matches[1])|add-content "$name.txt" #Get ip address
+ping google.com | where {$_ -match "Minimum"} |add-content "$name.txt" #add ping info
+[environment]::osversion | add-content "$name.txt"
+
+#promot for volume repair
+$Repair = Read-Host -Prompt "Run Repair Volume?(y/n): "
+if($Repair.ToUpper() -eq "Y")
+{
+Repair-volume -DriveLetter C -OfflineScanAndFix
+}
+
+#prompt for defrag
+$Defrag = Read-Host -Prompt "Run Defrag?(y/n): "
+if($Defrag.ToUpper() -eq "Y")
+{
+Optimize-Volume -DriveLetter C -Defrag -Verbose
+}
