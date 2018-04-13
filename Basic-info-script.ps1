b3c033294744ebb31787b5ef7d4acbb8c0a89c4e
JUrias117 <33735573+JUrias117@users.noreply.github.com>
2018-04-13 16:04:16 -0700

    Add files via upload
    Added Physical memory check

--- Basic-info-script.ps1
+++ Basic-info-script.ps1
@@ -6,6 +6,8 @@ Get-Date -Format g | Add-Content "$name.txt" #add date/time
 $(ipconfig | where {$_ -match 'IPv4.+\s(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' } | out-null; $Matches[1])|add-content "$name.txt" #Get ip address
 ping google.com | where {$_ -match "Minimum"} |add-content "$name.txt" #add ping info
 [environment]::osversion | add-content "$name.txt"
+$os = Get-WmiObject -class Win32_OperatingSystem
+"Free Space in GB: " + ([math]::round(($os.FreePhysicalMemory / 1024 / 1024), 2)) |add-content "$name.txt" #Gets free space 
 
 #promot for volume repair
 $Repair = Read-Host -Prompt "Run Repair Volume?(y/n): "
@@ -20,3 +22,7 @@ if($Defrag.ToUpper() -eq "Y")
 {
 Optimize-Volume -DriveLetter C -Defrag -Verbose
 }
+
+
+
+pause
